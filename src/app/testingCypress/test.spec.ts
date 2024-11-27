e082fc901d5af4039916a66018ffcc176bd21c58
CittGames <>
2024-11-27 11:52:28 -0300

    test eliminar

--- src/app/testingCypress/test.spec.ts
+++ src/app/testingCypress/test.spec.ts
@@ -1,25 +1,41 @@
 describe('My First Test', () => {
-    beforeEach(()=>{
-        cy.visit('/login')
-    })
-    it('primer test',()=>{
-        cy.contains('sesión')
-        cy.get('input[name="correo"]').should('be.visible')
-        cy.get('input[name="correo"]').type('plazadiego591@gmail.com')
-        cy.get('input[name="password"]').type('Dinop841')
-        cy.contains('niciar sesión con correo').click()
-        cy.url().should('include','/home')
-    })
-    it('segundo test',()=>{
-        cy.contains('sesión')
-        cy.get('input[name="correo"]').should('be.visible')
-        cy.get('input[name="correo"]').type('plazadiego591@gmail.com')
-        cy.get('input[name="password"]').type('Dinop841')
-        cy.contains('niciar sesión con correo').click()
-        cy.url().should('include','/home')
-        cy.contains('Programar viaje').click()
-        cy.get('input[name="Destino"').type('las vizcachas')
-        cy.contains('Buscar').click()
-        cy.get('ion-select')
-    })
-  })
+  beforeEach(() => {
+    cy.visit('/login');
+  });
+  it('Crear viaje', () => {
+    cy.contains('sesión');
+    cy.get('input[name="correo"]').should('be.visible');
+    cy.get('input[name="correo"]').type('plazadiego591@gmail.com');
+    cy.get('input[name="password"]').type('Dinop841');
+    cy.contains('Iniciar sesión Conductor').click();
+    cy.url().should('include', '/home');
+    cy.contains('Programar viaje').click();
+    cy.get('input[name="Destino"').type('las vizcachas');
+    cy.contains('Buscar').click();
+    cy.get('ion-select').click();
+    cy.contains('Las Vizcachas, Puente Alto');
+    cy.contains('OK').click();
+    cy.contains('Iniciar viaje').click();
+    cy.url().should('include', '/pro-d');
+    cy.get('input[name="capacidad"]').type('4');
+    cy.get('input[name="precio"]').type('5000');
+    cy.get('input[name="lugar_encuentro"]').type('Entrada');
+    cy.contains('Guardar').click();
+    cy.contains('Ver viajes').click();
+  });
+  it('Eliminar viaje', () => { 
+    cy.contains('sesión');
+    cy.get('input[name="correo"]').should('be.visible');
+    cy.get('input[name="correo"]').type('plazadiego591@gmail.com');
+    cy.get('input[name="password"]').type('Dinop841');
+    cy.contains('Iniciar sesión Conductor').click();
+    cy.url().should('include', '/home');
+    cy.url().should('include', '/home');
+    cy.contains('Ver viajes').click();
+    cy.get('ion-grid').should('be.visible');
+    cy.get('ion-grid ion-row').first().should('be.visible')
+    cy.get('ion-col').find('ion-button[name="eliminar"]').first().should('be.visible').click();
+    cy.contains('Eliminar').click();
+
+  });
+});
